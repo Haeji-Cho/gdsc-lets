481f835d83a64c8c7c168849f3b4a5c1d21409d6
Haeji-Cho <80144964+Haeji-Cho@users.noreply.github.com>
2023-01-14 01:20:45 +0900

    버튼으로 화면 전환

--- app/src/main/java/com/example/gdsc_lets/MainActivity2.java
+++ app/src/main/java/com/example/gdsc_lets/MainActivity2.java
@@ -2,7 +2,11 @@ package com.example.gdsc_lets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ImageButton imageButton=findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
