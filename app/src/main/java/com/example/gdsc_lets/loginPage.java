89b02218974a0c08242faa8802e138f1967b2916
Haeji-Cho <80144964+Haeji-Cho@users.noreply.github.com>
2023-01-17 15:20:42 +0900

    [2023.01.17]gdcs-lets

---
+++ app/src/main/java/com/example/gdsc_lets/loginPage.java
@@ -0,0 +1,30 @@
+package com.example.gdsc_lets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class loginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+
+        Button button12=findViewById(R.id.button12);
+        button12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
