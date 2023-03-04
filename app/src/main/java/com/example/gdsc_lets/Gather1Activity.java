f0bf8952e3b9aa47740bf4a6422b2072867b929c
Haeji-Cho <80144964+Haeji-Cho@users.noreply.github.com>
2023-03-05 02:00:20 +0900

    [23.03.04]
    사장님 페이지 추가, review 페이지 추가

---
+++ app/src/main/java/com/example/gdsc_lets/Gather1Activity.java
@@ -0,0 +1,14 @@
+package com.example.gdsc_lets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gather1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gather1);
+    }
+}
---
+++ app/src/main/java/com/example/gdsc_lets/ReserveDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.gdsc_lets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReserveDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve_detail);
+    }
+}
